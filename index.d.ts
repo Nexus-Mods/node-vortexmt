dbe0dc9243ac4bed546fbe74adf71760e036f5e3
TanninOne <seppleviathan@gmx.de>
2021-07-05 11:04:07 +0200

    typings fixed

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 // calculate md5 hash of a file
-export function fileMD5(filePath: string, callback: (err: Error, result: string) => void): void;
+export function fileMD5(filePath: string, callback: (err: Error, result: string) => void, progress: (progress: number, total: number) => void): void;
 
