2a74a7ecd2c128b39c23e04c7616b125db0b6783
TanninOne <seppleviathan@gmx.de>
2022-04-08 10:23:51 +0200

    better error message if opening file fails

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,47 @@ std::string toHex(uint8_t *buf, size_t len) {
   return result;
 }
 
+const char* errnoToString(int err) {
+  switch (err) {
+  case EPERM: return "EPERM";
+  case ENOENT: return "ENOENT";
+  case ESRCH: return "ESRCH";
+  case EINTR: return "EINTR";
+  case EIO: return "EIO";
+  case ENXIO: return "ENXIO";
+  case E2BIG: return "E2BIG";
+  case ENOEXEC: return "ENOEXEC";
+  case EBADF: return "EBADF";
+  case ECHILD: return "ECHILD";
+  case EAGAIN: return "EAGAIN";
+  case ENOMEM: return "ENOMEM";
+  case EACCES: return "EACCES";
+  case EFAULT: return "EFAULT";
+  case EBUSY: return "EBUSY";
+  case EEXIST: return "EEXIST";
+  case EXDEV: return "EXDEV";
+  case ENODEV: return "ENODEV";
+  case ENOTDIR: return "ENOTDIR";
+  case EISDIR: return "EISDIR";
+  case ENFILE: return "ENFILE";
+  case EMFILE: return "EMFILE";
+  case ENOTTY: return "ENOTTY";
+  case EFBIG: return "EFBIG";
+  case ENOSPC: return "ENOSPC";
+  case ESPIPE: return "ESPIPE";
+  case EROFS: return "EROFS";
+  case EMLINK: return "EMLINK";
+  case EPIPE: return "EPIPE";
+  case EDOM: return "EDOM";
+  case EDEADLK: return "EDEADLK";
+  case ENAMETOOLONG: return "ENAMETOOLONG";
+  case ENOLCK: return "ENOLCK";
+  case ENOSYS: return "ENOSYS";
+  case ENOTEMPTY: return "ENOTEMPTY";
+  default: return "UNKNOWN";
+  }
+}
+
 class MD5Worker : public Napi::AsyncProgressWorker<size_t> {
 public:
   MD5Worker(const std::string &filePath, const Napi::Function &progressCallback, const Napi::Function &appCallback)
@@ -31,6 +72,7 @@ public:
   virtual void Execute(const ExecutionProgress& progress) override {
     std::ifstream file(from_utf8(m_FilePath), std::ifstream::binary);
     if (!file.is_open()) {
+      m_ErrorCode = errno;
       SetError("Failed to open");
       return;
     }
@@ -71,6 +113,18 @@ public:
     Callback().Call(Receiver().Value(), std::initializer_list<napi_value>{ Env().Null(), Napi::String::New(Env(), m_Result.c_str()) });
   }
 
+  virtual void OnError(const Napi::Error& err) override {
+    std::string errMessage = strerror(m_ErrorCode);
+    const char* code = errnoToString(m_ErrorCode);
+
+    Napi::Env env = Env();
+    err.Value().Set("code", Napi::String::New(env, code));
+    err.Value().Set("errno", Napi::Number::New(env, m_ErrorCode));
+    err.Value().Set("path", Napi::String::New(env, m_FilePath));
+
+    Callback().Call(Receiver().Value(), std::initializer_list<napi_value>{ err.Value() });
+  }
+
   virtual void OnProgress(const size_t *data, size_t count) override {
     m_Progress.Call(Receiver().Value(), std::initializer_list<napi_value>{ Napi::Number::New(Env(), data[0]), Napi::Number::New(Env(), data[1]) });
   }
@@ -78,6 +132,7 @@ public:
 private:
   std::string m_FilePath;
   std::string m_Result;
+  int m_ErrorCode;
   Napi::FunctionReference m_Progress;
 };
 
